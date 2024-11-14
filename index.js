aad4fe257fd822f3f98c6b009a92fcf450e10c24
sadakat hossain <sadakat.sh1@gmail.com>
2024-11-14 21:49:55 +0700

    connect with mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -13,6 +14,38 @@ app.use(express.json());
 app.get('/', async (req, res) => {
     res.send("Poket School is running")
 });
+
+// Conndect with mongodb
+
+
+// const uri = "mongodb+srv://sadakathossain11:<db_password>@cluster3.c7qjq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3";
+const uri = "mongodb+srv://sadakathossain11:jKjrEOsAj2CZ3xUU@cluster3.c7qjq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3";
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();9
+    }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
     console.log(`Pocket School Server Is Runnig On port ${port}`);
